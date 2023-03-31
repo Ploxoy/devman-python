014c23ffe590de700c3a6d619db5e6401512c3ee
Ploxoy <pisem@kotenky.net>
2023-03-31 11:01:32 +0200

    .gitignore added

--- week1/weather.py
+++ week1/weather.py
@@ -1,16 +1,18 @@
 import requests
+
 url_template = 'https://wttr.in/{}'
-params = {'n': None,  # narrow version (only day and night)
-          'T': None,  # switch terminal sequences off (no colors)
-          'q': None,  # quiet version (no "Weather report" text)
-          'm': None,  # metric (SI) (used by default everywhere except US)
-          'M': None,  # show wind speed in m/s
-          'lang': 'ru'}
-# так как ключи с нулевым параметром не передаются в requests, преобразуем словарь  в строку:
-params = '&'.join([k if v is None else f"{k}={v}" for k, v in params.items()])
-cityes = ['Лондон', 'Шереметьево', 'Череповец']
-for city in cityes:
+params = {
+    'n':'',  # narrow version (only day and night)
+    'T':'',  # switch terminal sequences off (no colors)
+    'q':'',  # quiet version (no "Weather report" text)
+    'm':'',  # metric (SI) (used by default everywhere except US)
+    'M':'',  # show wind speed in m/s
+    'lang': 'ru',
+}
+
+cities = ['Лондон', 'Шереметьево', 'Череповец']
+for city in cities:
     url = url_template.format(city)
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=params, timeout=5)
     response.raise_for_status()
     print(response.text)
